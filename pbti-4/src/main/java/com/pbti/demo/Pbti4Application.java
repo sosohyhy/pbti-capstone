659b26848700903917b8339f4c0b26bd67e95e34
sosohyhy <tyui0984@naver.com>
2023-03-08 17:27:15 +0900

    프로젝트 초기화 완료

---
+++ pbti-4/src/main/java/com/pbti/demo/Pbti4Application.java
@@ -0,0 +1,13 @@
+package com.pbti.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pbti4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pbti4Application.class, args);
+	}
+
+}
---
+++ pbti-4/src/test/java/com/pbti/demo/Pbti4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbti4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
